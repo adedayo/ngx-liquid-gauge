4ba53d39924a2c0cd60323dd939a3f11e7865f87
jose alejandro godoy <desarrollo1@estacionmaritima.com>
2021-08-14 14:26:27 +0200

    suppourt for error with directives structurals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HostListener } from '@angular/core';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ngx-liquid-gauge-app';
   second = 22;
+  _innerWidth:number = 1000;
+  _posicionMouse:BehaviorSubject<{positionX: number,positionY: number}> = new BehaviorSubject<{positionX: number,positionY: number}>({positionX:0,positionY: 0})
+  posicionMouse$:Observable<{positionX: number,positionY: number}> = this._posicionMouse.asObservable();
+  @HostListener('document:mousemove', ['$event']) 
+  onMouseMove(e) {
+    this._posicionMouse.next({positionX: e.layerX,positionY:e.layerY})
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this._innerWidth = window.innerWidth;
+  }
   add(v:any) {
     this.second = v.target.value;
   }
